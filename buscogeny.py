89a08e42982800ccc0d10ca27f4f4a57fdeef706
John Webster <john.webster@dpi.nsw.gov.au>
2022-03-01 10:07:46 +1100

    Add BUSCO function
    Busogeny now also runs BUSCO as part of the pipeline.

--- buscogeny.py
+++ buscogeny.py
@@ -6,15 +6,26 @@ import argparse
 import pandas as pd
 import numpy as np
 import subprocess
+import shutil
+from version import __version__
 from Bio import SeqIO
 from alive_progress import alive_bar
 from Bio import AlignIO
-
+from Bio.Align import MultipleSeqAlignment
+from argparse import RawTextHelpFormatter
 
 class Buscogeny():
-	def __init__(self,dirs, odb):
-		self.dirs=dirs
+	def __init__(self,input, odb,threads):
+		self.input=input
 		self.odb=odb
+		self.threads=threads
+
+	def BUSCO(self,input,odb, threads):
+		if not os.path.exists("Buscogeny_out/BUSCO"):
+			os.mkdir("Buscogeny_out/BUSCO")
+		for file in os.listdir(input):
+			subprocess.run(['busco', '-i', input+file, '-o', os.path.basename(file)+'_busco_out','-c',threads,'-m', 'genome', '-l', odb])
+			shutil.move(os.path.basename(file)+'_busco_out',"Buscogeny_out/BUSCO/"+os.path.basename(file)+'_busco_out')
 
 	def database_parsing(self,odb):
 		if os.path.exists(os.path.join(odb,"links_to_ODB10.txt")):
@@ -22,27 +33,27 @@ class Buscogeny():
 			targets = df[0].tolist()
 			return(targets)
 
-	def busco_targs(self,targets,dirs,odb):
+	def busco_targs(self,targets,odb):
 		if not os.path.exists("Buscogeny_out/targets"):
 			os.mkdir("Buscogeny_out/targets")
-		n_isolates=len(os.listdir(dirs))
+		n_isolates=len(os.listdir("Buscogeny_out/BUSCO/"))
 		n_targs=len(targets)
 		with alive_bar(n_targs, title="Generating target multifastas") as bar:
 			for target in targets:
 				with open(f"Buscogeny_out/targets/{target}_all.faa","w") as out_h:
-					for dir in os.listdir(dirs):
+					for dir in os.listdir("Buscogeny_out/BUSCO/"):
 						db=os.path.basename(odb)
-						if os.path.exists(os.path.join(dirs,dir,"run_"+db,"busco_sequences","single_copy_busco_sequences",target+".faa")):
-							for seqrecord in SeqIO.parse(os.path.join(dirs,dir,"run_"+db,"busco_sequences","single_copy_busco_sequences",target+".faa"), "fasta"):
+						if os.path.exists(os.path.join("Buscogeny_out/BUSCO/",dir,"run_"+db,"busco_sequences","single_copy_busco_sequences",target+".faa")):
+							for seqrecord in SeqIO.parse(os.path.join("Buscogeny_out/BUSCO/",dir,"run_"+db,"busco_sequences","single_copy_busco_sequences",target+".faa"), "fasta"):
 								seqrecord.id=f"{dir}_{target}"
 								SeqIO.write(seqrecord, out_h, "fasta")
 					bar()
 
-	def target_alignments(self,dirs):
+	def target_alignments(self,input,threads):
 		if not os.path.exists("Buscogeny_out/alignments"):
 			os.mkdir("Buscogeny_out/alignments")
 		n_targs=len(os.listdir("Buscogeny_out/targets"))
-		n_isolates=len(os.listdir(dirs))
+		n_isolates=len(os.listdir(input))
 		with alive_bar(n_targs, title="Alignment completion status") as bar:
 			for file in os.listdir("Buscogeny_out/targets"):
 				(base,ext)=os.path.splitext(file)
@@ -51,7 +62,7 @@ class Buscogeny():
 					n+=1
 					if n==n_isolates:
 						with open(f"Buscogeny_out/alignments/{base}.aln","w") as out_h:
-							subprocess.run(['mafft','--adjustdirection','--thread','60',os.path.join("Buscogeny_out/targets",file)], stdout=out_h,stderr=subprocess.DEVNULL)
+							subprocess.run(['mafft','--adjustdirection','--thread',threads,os.path.join("Buscogeny_out/targets",file)], stdout=out_h,stderr=subprocess.DEVNULL)
 					else:
 						pass
 				bar()
@@ -59,13 +70,16 @@ class Buscogeny():
 	def alignment_concatenate(self):
 		if not os.path.exists("Buscogeny_out/supermatrix"):
 			os.mkdir("Buscogeny_out/supermatrix")
+		if not os.path.exists("Buscogeny_out/trimmed_alignments"):
+			os.mkdir("Buscogeny_out/trimmed_alignments")
 		i=0
+		matrix=[]
+		n_alns=len(os.listdir("Buscogeny_out/alignments"))
 		for file in os.listdir("Buscogeny_out/alignments"):
 			alignment = AlignIO.read(os.path.join("Buscogeny_out/alignments",file), "fasta")
 			alignment.sort()
 			for seqrecord in alignment:
 				seqrecord.id=seqrecord.id.split(".")[0]
-			# print("Alignment of length %i" % alignment.get_alignment_length())
 			if i==0:
 				cat_algn = alignment
 			else:
@@ -73,43 +87,50 @@ class Buscogeny():
 			i += 1
 		print("Concatenated:")
 		print("Alignment of length %i" % cat_algn.get_alignment_length())
-		outfh = open("Buscogeny_out/supermatrix/superalgn.clstl", "w")
+		outfh = open("Buscogeny_out/supermatrix/superaln.clstl", "w")
 		AlignIO.write(cat_algn, outfh, "clustal")
 		outfh.close()
+		subprocess.run(['clipkit',"Buscogeny_out/supermatrix/superaln.clstl",'-m','gappy','-o',"Buscogeny_out/supermatrix/superaln.degapped.clstl"])
 
 	def ML(self):
 		if not os.path.exists("Buscogeny_out/iqtree/"):
 			os.mkdir("Buscogeny_out/iqtree/")
-		# subprocess.run(['raxmlHPC', '-m', 'PROTGAMMAWAG', '-p', '1337', '-s', 'Buscogeny_out/supermatrix/superalgn.clstl', '-#', '1', '-n', 'output'])
-		subprocess.run(['iqtree', '-s', 'Buscogeny_out/supermatrix/superalgn.clstl', '-T', '60', '-B', '1000', '--prefix', 'Buscogeny_out/iqtree/iqtree'],stderr=subprocess.DEVNULL)
+		subprocess.run(['iqtree', '-s', "Buscogeny_out/supermatrix/superaln.degapped.clstl", "--keep-ident",'-T', '60', '-B', '1000', '--prefix', 'Buscogeny_out/iqtree/iqtree'])
 
 
 	def run(self):
 		if not os.path.exists("Buscogeny_out"):
 			os.mkdir("Buscogeny_out")
+		self.BUSCO(self.input,self.odb,self.threads)
 		targets=self.database_parsing(self.odb)
-		self.busco_targs(targets,self.dirs, self.odb)
-		self.target_alignments(self.dirs)
+		self.busco_targs(targets, self.odb)
+		self.target_alignments(self.input,self.threads)
 		self.alignment_concatenate()
 		self.ML()
 
 
 def main():
-	parser = argparse.ArgumentParser(description="Create Phylogenies from BUSCO output", add_help=False)
+	description=f"""
+	Create Phylogenies from BUSCO output. \n
+	Version: {__version__}"""
+
+	parser = argparse.ArgumentParser(description=description, formatter_class=RawTextHelpFormatter, add_help=False)
 
 	required = parser.add_argument_group('Required Arguments')
-	required.add_argument('-i', '--input', type=str, required=True, help="input folder of Busco output directories")
+	required.add_argument('-i', '--input', type=str, required=True, help="Input folder of Genomes")
 	required.add_argument('-d', '--db', type=str, required=True, help="Location of odb database")
 
 
 	optional = parser.add_argument_group('Optional Arguments')
 	optional.add_argument("-h", "--help", action="help", help="show this help message and exit")
+	optional.add_argument("-t", "--threads", type=str, required=False, default=8, help="Number of threads to use. Default 8")
 
 	args=parser.parse_args()
 	input=args.input
 	db=args.db
+	threads=args.threads
 
-	job = Buscogeny(input, db)
+	job = Buscogeny(input, db,threads)
 	job.run()
 
 
